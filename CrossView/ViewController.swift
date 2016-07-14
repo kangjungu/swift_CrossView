f8d971f2115fac66c379b9514903900446c39bfd
JHJG <kangjungu1@KangJunguui-MacBook-Pro.local>
2016-07-14 15:48:42 +0900

    swift_CrossView
    swift_CrossView

--- CrossView/ViewController.swift
+++ CrossView/ViewController.swift
@@ -1,18 +1,28 @@
-//
-//  ViewController.swift
-//  CrossView
-//
-//  Created by JHJG on 2016. 7. 4..
-//  Copyright © 2016년 KangJungu. All rights reserved.
-//
-
+/*
+ storyboard에 있는 label의 constraint를 삭제하고 (View b의 중앙 컨스트레인트)
+ button과 수평을 맞추는 예제
+ */
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel: UILabel!
+    @IBOutlet var myButton: UIButton!
+    @IBOutlet var viewB: UIView!
+    @IBOutlet var centerConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //constraint 삭제
+        viewB.removeConstraint(centerConstraint)
+        
+        //contraint 생성
+        let constraint = NSLayoutConstraint(item: myLabel,
+                                            attribute:NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: myButton, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
+        
+        self.view.addConstraint(constraint)
+        
     }
 
     override func didReceiveMemoryWarning() {
